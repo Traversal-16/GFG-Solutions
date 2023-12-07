0fc7be911b1b35de89f39aea7c3f890c476025d1
Shivanshu Katiyar <shivanshuworks16@gmail.com>
2023-12-07 20:45:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of subarrays with maximum values in given range/number-of-subarrays-with-maximum-values-in-given-range.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String []element = line1.trim().split(" ");
+		    int n = Integer.parseInt(element[0]);
+		    int l = Integer.parseInt(element[1]);
+		    int r = Integer.parseInt(element[2]);
+		    
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    System.out.println(obj.countSubarrays(arr, n, l, r));
+		        
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    static long countSubarrays(int a[], int n, int L, int R)
+    {
+         long ans=0;
+        int start=0;
+        int p=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=L && a[i]<=R)
+            {
+                p=i-start+1;
+            }
+            else if(a[i]>R)
+            {
+                start=i+1;
+                p=0;
+            }
+            ans=ans+p;
+        }
+        return ans;
+    } 
+}
+
